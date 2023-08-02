201e1907cf0b2d77637b2bcfa217d081a3c05bd0
tuz22 <seungji.dev@gmail.com>
2023-08-03 01:49:54 +0900

    이벤트핸들러

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,12 @@
         <!-- <태그 v-for="a" in 몇회" :key="a">{{a}}</태그> -->
         <h4 :style="titleStyle">{{ a }}</h4>
         <p>{{ price[i] }}만원</p>
+        <!--  v-on 축약: @ -->
+        <!-- <button v-on:click="report++">허위매물신고</button> -->
+        <button @click="report[i]++">허위매물신고</button>
+        <!-- 클릭 이벤트에 함수 적용 -->
+        <!-- <button @click="increase()">허위매물신고</button>  -->
+        <span>신고수 : {{ report[i] }}</span>
     </div>
 </template>
 
@@ -18,8 +24,16 @@ export default {
             menu: ['Home', 'Products', 'About'],
             price: [30, 60, 40],
             products: ['A 원룸', 'B 원룸', 'C 원룸'],
+            report: [0, 0, 0],
+            // report: 0,
             // titleStyle: 'color : blue', // HTML 속성도 데이터바인딩 가능
         };
+    },
+    methods: {
+        increase() {
+            // 버튼 클릭시 신고 수 증가
+            this.report++;
+        },
     },
     components: {},
 };
